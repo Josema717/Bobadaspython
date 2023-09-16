89dfdb7db46dfae6ae363471e414074bab111a2f
Josema <jose.almenarez@upb.edu.co>
2023-09-15 20:22:14 -0500

    Ya funciona tambien

--- lista con append.py
+++ lista con append.py
@@ -1,8 +1,9 @@
 numeros_pares = []
 n = int(input("Regalame un numero: "))
-contador = n 
-for numero in range(2, n+1, 2):
+contador = n
+lim= n*2 
+for numero in range(2, lim+1, 2):
     contador -= 1
-    numeros_pares = numeros_pares.append(numero)
-print(numeros_pares)
+    numeros_pares.append(numero)
+print(f"Los primeros {n} numeros pares son: " ,numeros_pares)
 
