f31f086956869854597f866c9f7eb5e5f36b6d54
Josema <jose.almenarez@upb.edu.co>
2023-10-04 20:56:22 -0500

    Ya funciona el codigo

--- practica3.py
+++ practica3.py
@@ -15,30 +15,33 @@ def crear_matriz(n):
 def intercambiar(matriz_nxn):
     for i in range(len(matriz_nxn)):
         for j in range(len(matriz_nxn[0])):
-            x = matriz_nxn[i][j]
-            if x%2 == 0:
+            if (matriz_nxn[i][j])%2 == 0:
                 matriz_nxn[i][j] = "X"
             else:
                 matriz_nxn[i][j] = "O"
-            return matriz_nxn
+    return matriz_nxn
 def main():
-    n = int(input("Ingrese un numero>>> "))
-    if n in range(5,11): 
-        matriz_nxn = crear_matriz(n) 
-        op = menu()
-        if op == 1:
-            for row in matriz_nxn:
-                for elem in row:
-                    print(elem, end='\t')
-                print()
-        elif op == 2:      
-            matriz_cambiada = intercambiar(matriz_nxn)
-            for row in matriz_cambiada:
-                for elem in row:
-                    print(elem, end='\t')
-                print()
-    else:
-        print("Por favor ingrese valores entre 5 y 11 :)")
-        
+    loop = True
+    while loop: 
+        n = int(input("Ingrese un numero>>> "))
+        if n in range(5,11): 
+            matriz_nxn = crear_matriz(n) 
+            op = menu()
+            if op == 1:
+                for row in matriz_nxn:
+                    for elem in row:
+                        print(elem, end='\t')
+                    print()
+            elif op == 2:      
+                matriz_cambiada = intercambiar(matriz_nxn)
+                for row in matriz_cambiada:
+                    for elem in row:
+                        print(elem, end='\t')
+                    print()
+            elif op == 3:
+                  loop = False 
+        else:
+            print("Por favor ingrese valores entre 5 y 11 :)")
+            
 if __name__ == "__main__":
     main()
